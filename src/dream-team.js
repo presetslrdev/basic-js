2f0f689292022e586c505428c8d4e6b646cb32c4
Denis <presetslr@gmail.com>
2020-02-16 21:26:46 +0300

    fix: dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,4 @@
 module.exports = function createDreamTeam( members ) {
   throw 'Not implemented';
-  return members.filter(el => typeof el === 'string').sort.map(el => el.slice(1)).join('');
+  return members ? members.filter(el => typeof el === 'string').sort.map(el => el.slice(1)).join('') : false;
 };
